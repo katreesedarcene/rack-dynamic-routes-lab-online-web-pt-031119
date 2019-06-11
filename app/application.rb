7019c7dd60f6d04de2bcf22594b6efa5cabde48a
Kat B <katreesebarnes@gmail.com>
2019-06-11 23:32:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
  
       song_title = req.path.split("/item/").last 
-      song = @@songs.find{|s| s.title == song_title}
+      item = @@items.find{|s| s.title == song_title}
  
       resp.write song.artist
     end
