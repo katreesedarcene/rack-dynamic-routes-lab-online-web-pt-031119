a1c14997bf30406cf7d289ed7bf973192bcd98b9
Kat B <katreesebarnes@gmail.com>
2019-06-11 23:45:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,26 +2,19 @@ class Application
  
   @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
  
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
- 
-    if req.path.match(/items/)
- 
-      song_title = req.path.split("/item/").last 
-     if item = @@items.find{|i| i.item == item_name}
- 
-      resp.write item.price
-      
-        else 
+   if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
         resp.status = 400
         resp.write "Item not found"
       end
-        else
+    else
       resp.status=404
       resp.write "Route not found"
     end
- 
     resp.finish
   end
+
 end
